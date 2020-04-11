96ae55924aadaec9084107e6057e658d14a12e13
fg91 <fabiograetz@googlemail.com>
2020-04-12 01:23:50 +0200

    Fibonacci project

---
+++ kubernetes_course/fibonacci/client/src/App.test.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+it('renders without crashing', () => {});
+// The reason we deleted the test is that the backend is not here during testing
+// Use a mockup
